b6549b748a72c73f33e6fc092f246849e61bfe06
JadahH <Jhol1918@students.vsu.edu>
2025-04-24 11:32:55 -0400

    Implemtented function

--- src/Param/Download.py
+++ src/Param/Download.py
@@ -6,4 +6,14 @@ from urllib.parse import urlparse
 import requests
 
 def parse(arvg=None):
-    pass
+   parser = argparse.ArgumentParser(
+        description="Download one or more files by URL, each on its own thread."
+    )
+   parser.add_argument(
+        "urls",
+        metavar="URL",
+        nargs='+',
+        help="HTTP(S) URL of the file to download",
+    )
+   return parser.parse(argv).urls
+
